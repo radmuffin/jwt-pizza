0b2bcc4b910422c9b7094121ef0a5b5f35502509
radmuffin <70724879+radmuffin@users.noreply.github.com>
2025-02-11 17:38:00 -0700

    mocked it, needs cleaning

--- tests/pizza.spec.js
+++ tests/pizza.spec.js
@@ -211,6 +211,162 @@ test("visit about and history", async ({ page }) => {
 });
 
 test("verify jwt", async ({ page }) => {
+	// part of this test is pretty redundant, also did a wrong password
+	await page.route("*/**/version.json", async (route) => {
+		const versionRes = { version: "1.0.0" };
+		await route.fulfill({ json: versionRes });
+	});
+
+	await page.route("*/**/api/order/menu", async (route) => {
+		const menuRes = [
+			{
+				id: 1,
+				title: "Veggie",
+				image: "pizza1.png",
+				price: 0.0038,
+				description: "A garden of delight",
+			},
+			{
+				id: 2,
+				title: "Pepperoni",
+				image: "pizza2.png",
+				price: 0.0042,
+				description: "Spicy treat",
+			},
+			{
+				id: 3,
+				title: "Margarita",
+				image: "pizza3.png",
+				price: 0.0042,
+				description: "Essential classic",
+			},
+			{
+				id: 4,
+				title: "Crusty",
+				image: "pizza4.png",
+				price: 0.0028,
+				description: "A dry mouthed favorite",
+			},
+			{
+				id: 5,
+				title: "Charred Leopard",
+				image: "pizza5.png",
+				price: 0.0099,
+				description: "For those with a darker side",
+			},
+		];
+		expect(route.request().method()).toBe("GET");
+		await route.fulfill({ json: menuRes });
+	});
+
+	await page.route("*/**/api/franchise", async (route) => {
+		const franchiseRes = [
+			{
+				id: 1,
+				name: "pizzaPocket",
+				stores: [
+					{
+						id: 1,
+						name: "SLC",
+					},
+				],
+			},
+		];
+		expect(route.request().method()).toBe("GET");
+		await route.fulfill({ json: franchiseRes });
+	});
+
+	await page.route("*/**/api/auth", async (route) => {
+		expect(route.request().method()).toBe("PUT");
+		const loginReq = { email: "d@jwt.com", password: "diner" };
+		const loginRes = {
+			user: {
+				id: 2,
+				name: "pizza diner",
+				email: "d@jwt.com",
+				roles: [
+					{
+						role: "diner",
+					},
+				],
+			},
+			token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwibmFtZSI6InBpenphIGRpbmVyIiwiZW1haWwiOiJkQGp3dC5jb20iLCJyb2xlcyI6W3sicm9sZSI6ImRpbmVyIn1dLCJpYXQiOjE3MzkzMDQwMTB9.m4hPKgKCJiQC60mI81XsEQ5EwTSTHjUgJBR702Kqnzw",
+		};
+		if (route.request().postDataJSON().password !== "diner") {
+			const failRes = {
+				message: "unknown user",
+				// stack: "Error: unknown user\n    at DB.getUser (C:\\Users\\spezd\\329\\jwt-pizza-service\\src\\database\\database.js:102:15)\n    at async C:\\Users\\spezd\\329\\jwt-pizza-service\\src\\routes\\authRouter.js:85:18",
+			};
+			await route.fulfill({ status: 404, json: failRes });
+		} else {
+			expect(route.request().postDataJSON()).toMatchObject(loginReq);
+			await route.fulfill({ json: loginRes });
+		}
+	});
+
+	await page.route("*/**/api/order", async (route) => {
+		if (route.request().method() === "POST") {
+			const orderReq = {
+				items: [
+					{
+						menuId: 4,
+						description: "Crusty",
+						price: 0.0028,
+					},
+				],
+				storeId: "1",
+				franchiseId: 1,
+			};
+			const orderRes = {
+				order: {
+					items: [
+						{
+							menuId: 4,
+							description: "Crusty",
+							price: 0.0028,
+						},
+					],
+					storeId: "1",
+					franchiseId: 1,
+					id: 9,
+				},
+				jwt: "eyJpYXQiOjE3MzkzMDQwMTIsImV4cCI6MTczOTM5MDQxMiwiaXNzIjoiY3MzMjkuY2xpY2siLCJhbGciOiJSUzI1NiIsImtpZCI6IjE0bk5YT21jaWt6emlWZWNIcWE1UmMzOENPM1BVSmJuT2MzazJJdEtDZlEifQ.eyJ2ZW5kb3IiOnsiaWQiOiJkYmFzMiIsIm5hbWUiOiJEYW5pZWwgU3BpZXNtYW4ifSwiZGluZXIiOnsiaWQiOjIsIm5hbWUiOiJwaXp6YSBkaW5lciIsImVtYWlsIjoiZEBqd3QuY29tIn0sIm9yZGVyIjp7Iml0ZW1zIjpbeyJtZW51SWQiOjQsImRlc2NyaXB0aW9uIjoiQ3J1c3R5IiwicHJpY2UiOjAuMDAyOH1dLCJzdG9yZUlkIjoiMSIsImZyYW5jaGlzZUlkIjoxLCJpZCI6OX19.A02IOgVX-qpL61EZrABbgsyKeUEAfQDyF8kYkF6sHUIr9N0FICjZhGT-G0vYdW0Vsi-4fDsMIjuNbyJ20aWKwlWHNWdzVCVjNAtF_8cUtQINv2K7JjtN0uRLCTp_80ilXbma7_SyrNi4icSJld5iWi5NOSADgFRFZhU_VEhIkIVbNUOvCPoPQUWmhQWLYG_5Y_q2aiNbHhnc3g4tCmL8KvB757wamURrUGaeeX_UQRteqVtRHa2rHpK0NVwCeD6F-rk_DVa0nCbE0rUS426Vs7wFprF587zitJ8DFmrKFrGbAXHMrmKaNdlZCavZATpZO2n9FqLd5JA_Kfk0sMYAE4pjNY5u8WhhYtPwmn00V8ZkWVIfUS2lUXx3PxbdOgjmUrIXhBppGk4sA9laIZfRwyrk-wJ5J7Ns7OBzwefB29Tbo_OqAO1r7l_QRt9wmgaXh_zi74uZR25x1Xe9QrvdhLIUgJZQApPqnGUpPZioVvNIrruFb7fDsV_nwkgxhcHX87s2XGRVTn6Zk5PIZuFln5KhQzAPi3DT8QUFvbSYdhqmUU31AM0GF7nacfUAZWu-lSXP-lD-PX5yR_mjJDjlg_UjrsizrmY6bhY3g2TqftiCv1V_OU0xz7z1CiMzX1I_dU9n0UzeOjjlEYPE35_u6j-iglW4hkqNlFvolFu3Nw0",
+			};
+			expect(route.request().postDataJSON()).toMatchObject(orderReq);
+			await route.fulfill({ json: orderRes });
+		} else if (route.request().method() === "GET") {
+			const orderRes = {
+				dinerId: 2,
+				orders: [
+					{
+						id: 1,
+						franchiseId: 1,
+						storeId: 1,
+						date: "2025-02-11T06:03:53.000Z",
+						items: [
+							{
+								id: 1,
+								menuId: 1,
+								description: "Veggie",
+								price: 0.0038,
+							},
+							{
+								id: 2,
+								menuId: 2,
+								description: "Pepperoni",
+								price: 0.0042,
+							},
+						],
+					},
+				],
+				page: 1,
+			};
+			await route.fulfill({ json: orderRes });
+		}
+	});
+
+	// there's another fetch to the factory but we should be chill leaving that one
+
 	await page.goto("http://localhost:5173/");
 	await page.getByRole("link", { name: "Order" }).click();
 	await page.getByRole("combobox").selectOption("1");
